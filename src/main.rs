e3068ef50d3c178234b5fd2441b3c154d98355d3
ozwaldorf <self@ossian.dev>
2024-10-11 14:35:51 -0400

    refactor: restructure handlers

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::io::{BufRead, Write};
-use std::time::SystemTime;
+use std::time::{Duration, SystemTime};
 
+use fastly::cache::simple::CacheEntry;
 use fastly::handle::BodyHandle;
-use fastly::http::{header, Method};
+use fastly::http::{Method, header};
 use fastly::kv_store::InsertMode;
-use fastly::{cache, Error, KVStore, Request, Response};
+use fastly::{Body, Error, KVStore, Request, Response, cache, mime};
 use humanize_bytes::humanize_bytes_binary;
 use humantime::format_duration;
 use pad::PadStr;
@@ -36,72 +37,23 @@ const PRIVACY_TEMPLATE: &str = include_str!("privacy.txt");
 
 #[fastly::main]
 fn main(req: Request) -> Result<Response, Error> {
-    // Log service version
     println!(
         "service version {}",
         std::env::var("FASTLY_SERVICE_VERSION").unwrap_or_default()
     );
 
-    Ok(match (req.get_method(), req.get_path()) {
-        (&Method::GET | &Method::HEAD, "/") => handle_usage(req)?,
-        (&Method::GET | &Method::HEAD, "/privacy") => handle_privacy(),
-        (&Method::GET | &Method::HEAD, _) => handle_get(req)?,
-        (&Method::PUT, _) => handle_put(req)?,
+    let res = match req.get_method() {
+        &Method::PUT => handle_put(req)?,
+        &Method::GET | &Method::HEAD => handle_get(req)?,
         _ => Response::from_status(403).with_body("invalid request"),
-    }
+    };
+
     // enable hsts, cross origin sharing, disable iframe embeds
-    .with_header(header::STRICT_TRANSPORT_SECURITY, "max-age=2592000")
-    .with_header(header::REFERRER_POLICY, "origin-when-cross-origin")
-    .with_header(header::ACCESS_CONTROL_ALLOW_ORIGIN, "*")
-    .with_header(header::X_FRAME_OPTIONS, "SAMEORIGIN"))
-}
-
-/// Handle a request to the usage page
-fn handle_usage(req: Request) -> Result<Response, Error> {
-    // Get hostname from request
-    let url = req.get_url();
-    let host = url.host().unwrap().to_string();
-    let max_line = HELP_TEMPLATE.lines().map(|l| l.len()).max().unwrap() + 2;
-
-    // Build header
-    let page = host.to_uppercase() + "(1)";
-    let title = "User Commands"
-        .pad_to_width_with_alignment(max_line - 2 * page.len(), pad::Alignment::Middle);
-    let header = format!("{page}{title}{page}");
-
-    // Build footer
-    let version = std::env!("CARGO_PKG_VERSION");
-    let mut footer = format!("{host} {version}");
-    footer += &compile_time::date_str!()
-        .pad_to_width_with_alignment(max_line - footer.len() - page.len(), pad::Alignment::Middle);
-    footer += &page;
-
-    let kv = KVStore::open(config::KV_STORE)?.expect("kv store to exist");
-    let upload_counter = get_upload_count(&kv);
-
-    // Render template
-    let mut tt = TinyTemplate::new();
-    tt.add_template("usage", HELP_TEMPLATE).unwrap();
-    let rendered = tt.render(
-        "usage",
-        &json!({
-            "header": header,
-            "host": host,
-            "max_size": *humanize_bytes_binary!(config::MAX_CONTENT_SIZE),
-            "kv_ttl": format_duration(config::KV_TTL).to_string(),
-            "cache_ttl": format_duration(config::CACHE_TTL).to_string(),
-            "upload_counter": upload_counter,
-            "footer": footer,
-        }),
-    )?;
-
-    // Respond with rendered helptext
-    Ok(Response::new().with_body_text_plain(&rendered))
-}
-
-/// Handle a request to the privacy policy page
-fn handle_privacy() -> Response {
-    Response::new().with_body_text_plain(PRIVACY_TEMPLATE)
+    Ok(res
+        .with_header(header::STRICT_TRANSPORT_SECURITY, "max-age=2592000")
+        .with_header(header::REFERRER_POLICY, "origin-when-cross-origin")
+        .with_header(header::ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+        .with_header(header::X_FRAME_OPTIONS, "SAMEORIGIN"))
 }
 
 /// Handle a request to put a paste into storage
@@ -154,64 +106,6 @@ fn handle_put(mut req: Request) -> Result<Response, Error> {
     Ok(Response::from_body(url + "\n").with_header("x-origin-url", origin_url))
 }
 
-/// Handle a request to get a paste
-fn handle_get(req: Request) -> Result<Response, Error> {
-    // Extract id from url
-    let mut segments = req.get_path().split('/').skip(1);
-    let id = segments.next().expect("empty path is handled earlier");
-    if id.len() != config::ID_LENGTH {
-        return Ok(Response::from_status(404).with_body("not found"));
-    }
-
-    let Ok((content, hash)) = get_cached_content(id) else {
-        return Ok(Response::from_status(404).with_body("not found"));
-    };
-
-    let origin_url = format!(
-        "https://{}/{id}#integrity=blake3-{hash}",
-        req.get_url().host().unwrap()
-    );
-
-    // Respond with content
-    Ok(Response::from_body(content)
-        .with_header("x-origin-url", origin_url)
-        .with_header(
-            // Client-side cache control, content will never change
-            header::CACHE_CONTROL,
-            "public, s-maxage=31536000, immutable",
-        ))
-}
-
-/// Get immutable content from the cache, or fallback to kv store and insert to cache.
-fn get_cached_content(id: &str) -> Result<(BodyHandle, String), Error> {
-    let key = "file_".to_string() + id;
-
-    // Try to find content in cache
-    if let Some(found) = cache::core::lookup(key.clone().into()).execute()? {
-        let meta = found.user_metadata();
-        let hash = String::from_utf8(meta.to_vec()).unwrap();
-
-        Ok((found.to_stream()?.into_handle(), hash))
-    } else {
-        // Otherwise, get content from key value store (origin)
-        let kv = KVStore::open(config::KV_STORE)?.expect("kv store to exist");
-        let mut res = kv.lookup(&key)?;
-        let meta = res.metadata().unwrap();
-        let hash = String::from_utf8(meta.to_vec()).unwrap();
-        let content = res.take_body_bytes();
-
-        // Write content to cache
-        let mut w = cache::core::insert(key.to_owned().into(), config::CACHE_TTL)
-            .surrogate_keys(["get"])
-            .user_metadata(meta)
-            .execute()?;
-        w.write_all(&content)?;
-        w.finish()?;
-
-        Ok((content.into(), hash))
-    }
-}
-
 /// Get upload count from the metadata, or fallback to the number of metric lines.
 fn get_upload_count(kv: &KVStore) -> usize {
     kv.lookup(config::UPLOAD_METRICS_KEY)
@@ -243,4 +137,126 @@ fn track_upload(kv: &KVStore, id: &str, file: &str) -> Result<(), Error> {
             ),
         )?;
     Ok(())
+}
+
+/// Handle a request to get a paste
+fn handle_get(req: Request) -> Result<Response, Error> {
+    match req.get_url().path_segments().unwrap().next() {
+        // Usage page
+        None => {
+            let host = req.get_url().host().unwrap().to_string();
+            let usage = get_usage(host)?;
+            Ok(Response::from_body(usage).with_content_type(mime::TEXT_PLAIN_UTF_8))
+        },
+
+        // Privacy policy page
+        Some("privacy") => {
+            Ok(Response::from_body(PRIVACY_TEMPLATE).with_content_type(mime::TEXT_PLAIN_UTF_8))
+        },
+
+        // Paste download
+        Some(id) if id.len() == config::ID_LENGTH => {
+            let Ok((content, hash)) = get_paste(id) else {
+                return Ok(Response::from_status(404).with_body("not found"));
+            };
+
+            let origin_url = format!(
+                "https://{}/{id}#integrity=blake3-{hash}",
+                req.get_url().host().unwrap()
+            );
+
+            // Respond with content
+            Ok(Response::from_body(content)
+                .with_header("x-origin-url", origin_url)
+                .with_header(
+                    // Client-side cache control, content will never change
+                    header::CACHE_CONTROL,
+                    "public, s-maxage=31536000, immutable",
+                ))
+        },
+
+        // Unknown path
+        _ => Ok(Response::from_status(404).with_body("not found")),
+    }
+}
+
+/// Handle a request to the usage page
+fn get_usage(host: String) -> Result<Body, Error> {
+    let body = cache::simple::get_or_set_with(host.clone(), || {
+        // Compute max line
+        let max_line = HELP_TEMPLATE.lines().map(|l| l.len()).max().unwrap() + 2;
+
+        // Build header
+        let page = host.to_uppercase() + "(1)";
+        let title = "User Commands"
+            .pad_to_width_with_alignment(max_line - 2 * page.len(), pad::Alignment::Middle);
+        let header = format!("{page}{title}{page}");
+
+        // Build footer
+        let version = std::env!("CARGO_PKG_VERSION");
+        let mut footer = format!("{host} {version}");
+        footer += &compile_time::date_str!().pad_to_width_with_alignment(
+            max_line - footer.len() - page.len(),
+            pad::Alignment::Middle,
+        );
+        footer += &page;
+
+        // Get upload counter
+        let kv = KVStore::open(config::KV_STORE)?.expect("kv store to exist");
+        let upload_counter = get_upload_count(&kv);
+
+        // Render template
+        let mut tt = TinyTemplate::new();
+        tt.add_template("usage", HELP_TEMPLATE).unwrap();
+        let rendered = tt.render(
+            "usage",
+            &json!({
+                "header": header,
+                "host": host,
+                "max_size": *humanize_bytes_binary!(config::MAX_CONTENT_SIZE),
+                "kv_ttl": format_duration(config::KV_TTL).to_string(),
+                "cache_ttl": format_duration(config::CACHE_TTL).to_string(),
+                "upload_counter": upload_counter,
+                "footer": footer,
+            }),
+        )?;
+
+        // Cache homepage for 5 minutes
+        Ok(CacheEntry {
+            value: rendered.into(),
+            ttl: Duration::from_secs(5 * 60),
+        })
+    })?
+    .expect("cache to have a body");
+    Ok(body)
+}
+
+/// Get immutable content from the cache, or fallback to kv store and insert to cache.
+fn get_paste(id: &str) -> Result<(BodyHandle, String), Error> {
+    let key = "file_".to_string() + id;
+
+    // Try to find content in cache
+    if let Some(found) = cache::core::lookup(key.clone().into()).execute()? {
+        let meta = found.user_metadata();
+        let hash = String::from_utf8(meta.to_vec()).unwrap();
+
+        Ok((found.to_stream()?.into_handle(), hash))
+    } else {
+        // Otherwise, get content from key value store (origin)
+        let kv = KVStore::open(config::KV_STORE)?.expect("kv store to exist");
+        let mut res = kv.lookup(&key)?;
+        let meta = res.metadata().unwrap();
+        let hash = String::from_utf8(meta.to_vec()).unwrap();
+        let content = res.take_body_bytes();
+
+        // Write content & metadata to cache
+        let mut w = cache::core::insert(key.to_owned().into(), config::CACHE_TTL)
+            .surrogate_keys(["get"])
+            .user_metadata(meta)
+            .execute()?;
+        w.write_all(&content)?;
+        w.finish()?;
+
+        Ok((content.into(), hash))
+    }
 }
